b9ee92da31c1beb4904d269e68be61e92ef2fa04
maxior1 <152420825+maxior1@users.noreply.github.com>
2023-12-06 22:53:07 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,30 @@
+const header = document.querySelector("header");
+
+window.addEventListener ("scroll", function(){
+    header.classList.toggle ("sticky", window.scrollY >0);
+});
+
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('open');
+}
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navlist.classList.remove('open');
+}
+
+const sr = ScrollReveal ({
+    distance: '30px',
+    duration: 2600,
+    reset: true
+})
+
+sr.reveal('.home-text',{delay:200, origin:'bottom'})
+sr.reveal('.featured, .brand',{delay:200, origin:'top'})
+sr.reveal('.cta',{delay:200, origin:'left'})
+sr.reveal('.new',{delay:280, origin:'right'})
+sr.reveal('.contact',{delay:200, origin:'bottom'})
